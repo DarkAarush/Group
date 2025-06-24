d9c68727103ceb39e62d3085c87d1a26c915b421
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-06-24 14:11:23 +0530

    Update gr.py

--- gr.py
+++ gr.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 
-api_id = int(input("Enter your API ID: 18499702"))
-api_hash = input("Enter your API HASH: d4dff36c2c1ebf6f8f6bc044b5bce9c9")
+api_id = int(input("Enter your API ID: "))
+api_hash = input("Enter your API HASH: ")
 
 with Client("my_session", api_id=api_id, api_hash=api_hash) as app:
     print("Your string session:")
